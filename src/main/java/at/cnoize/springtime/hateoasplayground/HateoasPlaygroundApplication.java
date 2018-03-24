01bfe653dee2c155e5e6c5b181a1601bc1c98970
Matthias Holzinger <matthias.holzinger@catalysts.cc>
2018-03-24 12:16:32 +0100

    start.spring.io - web,hateoas,actuator,devtools

---
+++ src/main/java/at/cnoize/springtime/hateoasplayground/HateoasPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package at.cnoize.springtime.hateoasplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasPlaygroundApplication.class, args);
+	}
+}
